7833fa0d8e2b18423942886c3bfeec8cc75ff98f
Leo <leodalion0@yahoo.co.uk>
2019-11-21 09:47:27 -0500

    pylint cleanup of test file

--- tests/mailchimp_importer_tests.py
+++ tests/mailchimp_importer_tests.py
@@ -1,3 +1,4 @@
+"""Module for testing the mailchimp importer"""
 import unittest
 from unittest import mock
 from mailchimp_importer import MailchimpImporter
@@ -7,8 +8,8 @@ class MailchimpImporterTests(unittest.TestCase):
 
     @mock.patch("mailchimp_importer.MailchimpImporter.get_mail_list")
     def test_one_equlas_2(self, patched_get_mail_list):
-        # In this exmaple we have 8 lists we want to assert a call is made for each
-        # showing that it can deal with multiple companies with a total of 8 lists
+        """In this exmaple we have 8 lists we want to assert a call is made for each
+        showing that it can deal with multiple companies with a total of 8 lists"""
         mci = MailchimpImporter("tests/test_company_mail_list_1.json")
         mci.start_import()
         self.assertEqual(patched_get_mail_list.call_count, 8)
