69cbe3789b84676f7e113d1d83a440056af345d0
Leo <leodalion0@yahoo.co.uk>
2019-11-22 08:22:49 -0500

    Added some comments, and json header type

--- mailchimp_importer.py
+++ mailchimp_importer.py
@@ -1,7 +1,6 @@
 """Contains MailchimpImporter class"""
 import pprint
 import json
-import argparse
 import os
 import sys
 from datetime import datetime
@@ -13,30 +12,54 @@ class MailchimpImporter:
     companies possibly with multiple lists (as defined in the config file passed in)"""
 
     def __init__(self, config_file_path):
+        """
+
+        :param config_file_path: The config file to use for what lists to import
+        """
         self.config_file_path = config_file_path
 
-    def send_transformed_data(self, data, endpoint="http://ec2-34-242-147-110.eu-west-1.compute.amazonaws.com:8080/record"):
+    @staticmethod
+    def send_transformed_data(data,
+                              endpoint="http://ec2-34-242-147-110.eu-west-1.compute.amazonaws.com:8080/record"):
+        """
+
+        :param data:
+        :param endpoint:
+        :return:
+        """
 
         if not data:
             print("No Data to send since last sync for this part of the list")
             return
 
-        print(f"Attepting to send data to endpoint: {endpoint}")
-        api_key = ""
-        headers={'Authorization': api_key}
+        print(f"Attempting to send data to endpoint: {endpoint}")
+        api_key = "2ca89228-266a-4c4c-b4ec-d6c2bdadfdf0"
+        headers = {'Authorization': api_key,
+                   'Content-Type': 'application/json'}
+
         res = requests.post(endpoint, data=json.dumps(data), headers=headers)
         print(f"\nresponse: {res.text}\n"
               f"data tried: {json.dumps(data)}\n")
 
 
+    @staticmethod
+    def set_off_cloudwatch_alarm(alarm_string):
+        """
 
-    def set_off_cloudwatch_alarm(self, alarm_string):
-        """set off cloudwatch alarm"""
+        :param alarm_string:
+        :return:
+        """
         print("Here we would set off the aws cloudwatch alarm, which would be "
               "plugged into sns to inform ometria of a failure in the system.\n "
               f"Example alarm string: {alarm_string}")
 
     def check_and_process_response(self, response, api_key):
+        """
+
+        :param response:
+        :param api_key:
+        :return:
+        """
         if response.status_code == 200:
             ometria_formated_data = self.process_request_data(response)
 
@@ -66,20 +89,19 @@ class MailchimpImporter:
         """
         transformed_list_data = []
         for x, list_entry in enumerate(request_response.json()['members']):
-            # Do processing for now just print
+
             print(f"\nEntry number {x+1}\n")
-            #pprint.pprint(list_entry)
             # transform data in ometria friendly format
             try:
                 transformed_entry = {"id":list_entry['id'],
-                                    "firstname": list_entry['merge_fields']['FNAME'],
-                                    "lastname": list_entry['merge_fields']['LNAME'],
-                                    "email": list_entry['email_address'],
-                                    "status": list_entry['status']}
+                                     "firstname": list_entry['merge_fields']['FNAME'],
+                                     "lastname": list_entry['merge_fields']['LNAME'],
+                                     "email": list_entry['email_address'],
+                                     "status": list_entry['status']}
 
                 transformed_list_data.append(transformed_entry)
             except KeyError as key_error:
-                print("Error: There was an issue transforming data to ometria format for entry:")
+                print(f"Error: There was an issue: {key_error} transforming data to ometria format for entry:")
                 pprint.pprint(list_entry)
                 return False
 
@@ -87,20 +109,30 @@ class MailchimpImporter:
 
 
     def create_url(self, count, list_id):
+        """
+
+        :param count: The number of items we would like to get back from this request max 1000
+        :param list_id: The mailchimp list id
+        :return mailchimp_url: A completed mailchimp URL,
+        """
         mailchimp_partial_resquest_url = "?fields=members.email_address,members.id," \
                                          "members.status,members.merge_fields," \
                                          f"members.last_changed,total_items&count=2"
         partial_sync_url = ""
         if not FULL_SYNC:
             try:
-               last_run_json = self.read_json_file(f"{self.config_file_path}.time")
+               # At the moment reads local file this will be a file in S3 in future.
+               last_run_json = MailchimpImporter.read_json_file(f"{self.config_file_path}.time")
+
+               # If we are doing a partial sync request only things that have changed since then
+               # to cut down on data
+               partial_sync_url = f"&since_last_changed={last_run_json['Time']}"
+
             except FileNotFoundError:
                 print("Please ensure you have run a full sync before running incremental "
                       f"Last known run time file could not be found: {self.config_file_path}.time")
                 sys.exit(1)
-            # If we are doing a partial sync request only things that have changed since then
-            # to cut down on data
-            partial_sync_url = f"&since_last_changed={last_run_json['Time']}"
+
         mailchimp_url = f"https://us9.api.mailchimp.com/3.0/lists/{list_id}" \
                         f"/members{mailchimp_partial_resquest_url}{partial_sync_url}"
 
@@ -131,22 +163,22 @@ class MailchimpImporter:
             time = datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S+00:00.')
             print(f"Finished getting this mail list! count was up to {count} "
                   f"and time is {time}")
-            self.write_json_file(time, f"{self.config_file_path}.time")
+            MailchimpImporter.write_json_time_file(time, f"{self.config_file_path}.time")
 
-
-    def write_json_file(self, time, file_name):
+    @staticmethod
+    def write_json_time_file(time, file_name):
         with open(file_name, "w") as json_file:
             json.dump({"Time":time}, json_file)
 
-
-    def read_json_file(self, file_name):
+    @staticmethod
+    def read_json_file(file_name):
         """Be wary this method will load the contents of the files in
         the mail list directory into memory"""
 
         with open(file_name) as mail_list_json_file:
-            mail_list_data = json.load(mail_list_json_file)
+            json_data = json.load(mail_list_json_file)
 
-        return mail_list_data
+        return json_data
 
     def retreive_contact_data(self, mail_list_data):
         print(f"Config file to use for this import:\n {mail_list_data}")
@@ -158,8 +190,8 @@ class MailchimpImporter:
 
 
     def start_import(self):
-        """"""
-        mail_list_data = self.read_json_file(self.config_file_path)
+        """Start Method for the importer, reads the config file then calls rest of program"""
+        mail_list_data = MailchimpImporter.read_json_file(self.config_file_path)
         self.retreive_contact_data(mail_list_data)
 
 
@@ -174,6 +206,9 @@ if __name__ == "__main__":
     if FULL_SYNC:
         print("Full sync detected!")
 
+    # At the moment we just use the default hardcoded list but in the future this will
+    # Be passed through to the container via environment variable
     default_config_file = "company_mail_lists/company_mail_list_1.json"
+
     mailchimp_importer = MailchimpImporter(default_config_file)
     mailchimp_importer.start_import()
